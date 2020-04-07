d895c3e6bdedf9089e06130609a486ff35933a4a
Nicholas Chung <iamnicholaschung@gmail.com>
2020-04-07 17:15:56 +0800

    Move EventDetailsPage

---
+++ src/Component/EventDetailsPage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import axios from "axios";
+import ReactLoading from "react-loading";
+import { menu } from "semantic-ui-react";
+class EventDetailsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullEventData: "",
+      isloading: true,
+      eventId: this.props.match.params.id,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://dancer-network.herokuapp.com/events/${this.state.eventId}`)
+      .then((res) => {
+        this.setState({
+          fullEventData: res.data,
+          isloading: false,
+        });
+      })
+      .catch(() =>
+        this.setState({
+          errorMessage: "GG liao",
+          isloading: false,
+        })
+      );
+  }
+
+  render() {
+    const fullDetails = this.state.fullEventData;
+    return (
+      <div>
+        {!!this.state.isloading && <ReactLoading />}
+        <div className="fullDisplayCard">
+          <div className="fullDisplayTextContainer">
+            <h2>{this.state.fullEventData.eventName}</h2>
+            <p>Location :{fullDetails.location}</p>
+            <p>Start Date: {fullDetails.eventStartDate}</p>
+            <p>End Date: {fullDetails.eventEndDate}</p>
+            <p>danceStyle: {fullDetails.danceStyle}</p>
+            <p>{fullDetails.eventSummary}</p>
+            <h4>By: {fullDetails.eventOwner}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default EventDetailsPage;
